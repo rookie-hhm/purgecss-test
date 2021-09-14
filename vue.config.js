3f2cb7b72c26257357087f47f767c5e96c391250
hhm-lxb <lxbmisszsj@gmail>
2021-09-14 10:17:44 +0800

    test

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,9 @@
 // const PurgecssPlugin = require('purgecss-webpack-plugin')
 // const glob = require('glob-all')
-// const path = require('path')
-// const resolve = (dir) => {
-//   return path.join(__dirname, dir)
-// }
+const path = require('path')
+const resolve = (dir) => {
+  return path.join(__dirname, dir)
+}
 // class TailwindExtractor {
 //   static extract (content) {
 //     return content.match(/[A-z0-9-_:\/]+/g) || []
@@ -29,4 +29,26 @@
 //     // }
 //   }
 // }
-module.exports = {}
+const PurgecssPlugin = require('@fullhuman/postcss-purgecss')
+
+module.exports = {
+  chainWebpack (config) {
+    if (process.env.NODE_ENV === 'production') {
+      config.plugin('PurgecssPlugin')
+        .use(PurgecssPlugin, [
+          {
+            content: [
+              `${resolve('public')}/**/*.html`,
+              `${resolve('src')}/**/*.vue`,
+              `${resolve('src')}/**/*.js`
+            ],
+            defaultExtractor (content) {
+              const contentWithoutStyleBlocks = content.replace(/<style[^]+?<\/style>/gi, '')
+              return contentWithoutStyleBlocks.match(/[A-Za-z0-9-_/:]*[A-Za-z0-9-_/]+/g) || []
+            },
+            safelist: [/-(leave|enter|appear)(|-(to|from|active))$/, /^(?!(|.*?:)cursor-move).+-move$/, /^router-link(|-exact)-active$/, /data-v-.*/]
+          }
+        ])
+    }
+  }
+}
